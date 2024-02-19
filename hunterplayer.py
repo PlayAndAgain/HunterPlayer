d2af4ad1c50cea2d2fb895e1ae39c53d016836bd
PlayAndAgain <158172460+PlayAndAgain@users.noreply.github.com>
2024-02-19 16:49:35 +0100

    Add files via upload

---
+++ hunterplayer.py
@@ -0,0 +1,18 @@
+import keyboard
+import pyautogui as gui
+import tkinter as tk
+class HunterPlayer:
+    def __init__(self):
+        self.fenetre= tk.Tk()
+        self.haut_gauche = None
+        self.bas_droit = None
+        self.centre = None
+        self.actif = False
+        self.fenetre.mainloop()
+    
+    def intiation_fenêtre():
+        pass
+
+HunterPlayer()
+        
+    
